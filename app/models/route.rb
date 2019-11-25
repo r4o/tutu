95f614765557f1e2a040b408ec91a79636dec468
PB <r4o7r9@gmail.com>
2019-11-25 16:03:09 +0300

    r

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,14 @@
 class Route < ApplicationRecord
   has_many :railway_stations_routes
   has_many :railway_stations, through: :railway_stations_routes
+
+  validates :name, presence: true
+
+  before_create :set_name
+
+  private
+
+  def set_name
+    self.name = "#{railway_stations.first.title} - #{railway_stations.last.title}"
+  end
 end
