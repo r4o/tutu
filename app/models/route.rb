e0e62ce56f05256a722313fde2a37db2b0e474fc
PB <r4o7r9@gmail.com>
2019-11-25 19:43:41 +0300

    Add models

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,6 +3,7 @@ class Route < ApplicationRecord
   has_many :railway_stations, through: :railway_stations_routes
 
   validates :name, presence: true
+  validate :stations_count
 
   before_validation :set_name
 
@@ -10,5 +11,11 @@ class Route < ApplicationRecord
 
   def set_name
     self.name ||= "#{railway_stations.first.title} - #{railway_stations.last.title}"
+  end
+
+  def stations_count
+    if railway_stations.size < 2
+      errors.add(:base, "Route should contain at least 2 stations")
+    end
   end
 end
