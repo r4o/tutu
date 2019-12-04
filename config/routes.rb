9ccb3e1b18a67fe6160f3dd476a5ed2a21aef0ae
PB <r4o7r9@gmail.com>
2019-12-04 16:35:07 +0300

    17

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :tickets, only: [:create, :show]
-
-  resources :railway_stations do
-    patch :update_position, on: :member
-  end
-  resources :trains do
-    resources :carriages, shallow: true
-  end
-
-  resources :routes
-
   resource :search, only: [:new, :show, :edit]
+
+  namespace :admin do
+    resources :railway_stations do
+      patch :update_position, on: :member
+    end
+
+    resources :trains do
+      resources :carriages, shallow: true
+    end
+
+    resources :routes
+    resources :tickets
+  end
 
   get 'welcome/index'
 
