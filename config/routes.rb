2badccc92fb7e5d6be5985836dabcdf0a534bc9e
PB <r4o7r9@gmail.com>
2019-12-11 20:54:23 +0300

    18

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,5 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
 
-  root 'searhces#show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
 end
